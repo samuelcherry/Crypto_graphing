b1408c5088b43d264bc10a834f60934d1f6bfde6
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-11-18 22:43:27 -0700

    added highest, lowest and avg numbers. Need to change highest and lowest on graph to be highest of last hour plus 10 percent. Same with lower

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const priceEl = document.getElementById("price");
 const lastFiftyElement = document.getElementById("priceArray");
+const highest = document.getElementById("highest");
+const lowest = document.getElementById("lowest");
+const avgPercent = document.getElementById("avgPercent");
 
 // Binance BTCUSDT Trade Stream
 const socket = new WebSocket("wss://data-stream.binance.com/ws/btcusdt@trade");
@@ -7,25 +10,51 @@ const socket = new WebSocket("wss://data-stream.binance.com/ws/btcusdt@trade");
 let lastFifty = [];
 let lastFiftyAvg = 0;
 
+let currentMinutePrices = [];
+let minuteAverages = [];
+
 socket.onopen = () => {
   console.log("Connected to Binance WebSocket!");
 };
 
 socket.onmessage = (event) => {
   const data = JSON.parse(event.data);
+  const price = parseFloat(data.p);
+  currentMinutePrices.push(price);
 
-  // price is in 'p'
-  const price = parseFloat(data.p).toFixed(2);
   priceEl.textContent = "$" + price;
 
   if (lastFifty.length >= 50) {
     lastFifty.shift();
   }
   lastFifty.push(parseFloat(data.p));
-  let sum = lastFifty.reduce((sum, num) => sum + num, 0);
-  lastFiftyElement.textContent = sum / lastFifty.length;
+
+  let highestNumber = Math.max(...lastFifty);
+  let lowestNumber = Math.min(...lastFifty);
+  let avg = minuteAverages[minuteAverages.length - 1];
+
+  let newHighest = highestNumber - lowestNumber;
+  let newAvg = avg - lowestNumber;
+  let avgDec = newAvg / newHighest;
+  let avgPer = avgDec;
+
+  highest.textContent = highestNumber;
+  lowest.textContent = lowestNumber;
+  avgPercent.textContent = minuteAverages.length ? avgPer : "0";
 };
 
 socket.onerror = (err) => {
   console.error("WebSocket Error:", err);
 };
+
+setInterval(() => {
+  if (currentMinutePrices.length === 0) return;
+
+  const sum = currentMinutePrices.reduce((a, b) => a + b, 0);
+  const avg = sum / currentMinutePrices.length;
+
+  minuteAverages.push(avg);
+
+  currentMinutePrices = [];
+  console.log("New 1-minute average:", avg);
+}, 60000);
